68535a85fa07f75a41a9fd8f63aa0f878950eccf
Ana - Betatel <anamarija.boshnjak@gmail.com>
2021-04-10 20:27:29 +0200

    Forecast data added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error){
             callback(body.error, undefined)
         }else {
-            callback(undefined, body.current.weather_descriptions[0] +". It is currently " + body.current.temperature + " degrees out. It feels like "+ body.current.feelslike)
+            callback(undefined, body.current.weather_descriptions[0] +". It is currently " + body.current.temperature + " degrees out. It feels like "+ body.current.feelslike + ". It is " + body.current.observation_time)
         }
     })
 }
